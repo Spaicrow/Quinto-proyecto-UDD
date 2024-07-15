4eb5fd6325e9dac39dfbaa6b537b079fa9df5904
eliseoHinojosa <eliseohinojosa@hotmail.es>
2024-07-15 13:08:56 -0400

    pokedex second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import Asaid from "./components/Asaid";
+import Pokemons from "./components/Pokemons";
 
 function App() {
   return (
-    <>
-      <h1>Pokedex</h1>
-    </>
+    <section className="bg-[#f6f8fc]">
+      <main className="max-w-[1400px] mx-auto grid grid-cols-1 lg:grid-cols-[1fr_350px]
+       h-screen font-outfit">
+        <Pokemons/>
+        <Asaid />
+      </main>
+    </section>
   )
 }
 
---
+++ src/components/Asaid.jsx
@@ -0,0 +1,10 @@
+ 
+const Asaid = () => {
+  return (
+    <div className="hidden lg:block">
+      Asaid
+    </div>
+  )
+}
+
+export default Asaid
